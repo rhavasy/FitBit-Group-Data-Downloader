36e9a74a608549796aaa4978520c853e9bc86f5c
Rob Havasy <rob@havasy.net>
2013-01-04 10:57:19 -0500

    remove fitbit-authenticate.py

--- fitbit-authenticate.py
+++
@@ -1,9 +0,0 @@
-import fitbit
-f=fitbit.FitBit()
-PIN = 'pr8f78a3msncolh3lo8g86jqbv'
-
-auth_url, auth_token = f.GetRequestToken()
-
-access_token = f.GetAccessToken(PIN, auth_token)
-
-response = f.ApiCall(access_token, apiCall='/1/user/-/activities/log/steps/date/today/7d.json')
