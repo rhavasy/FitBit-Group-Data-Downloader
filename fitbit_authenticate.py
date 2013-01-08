04bddcc9afe83d913a9c4e44c63742f133d4df01
Alyssa Woulfe <awoulfe@partners.org>
2013-01-08 14:19:04 -0500

    Automated the script to prompt for pin

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -1,9 +1,16 @@
+# Modified by Alyssa Woulfe to automate the authentication workflow
+
+
 import fitbit
 f=fitbit.FitBit()
-PIN = 'pr8f78a3msncolh3lo8g86jqbv'
 
 auth_url, auth_token = f.GetRequestToken()
+
+print 'Please open your web browser and go to the copy the following URL to obtain your pin: %s' %auth_url
+
+PIN = raw_input("Please copy the pin that is returned from Fitbit: ")
 
 access_token = f.GetAccessToken(PIN, auth_token)
 
 response = f.ApiCall(access_token, apiCall='/1/user/-/activities/log/steps/date/today/7d.json')
+print response
