c80cd057fa85836ac1e80b69f970033a9f08d718
Alyssa Woulfe <awoulfe@partners.org>
2013-01-09 13:08:42 -0500

    Integrated PickApiCall & fixed line spacing to be more user friendly

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -6,11 +6,17 @@ f=fitbit.FitBit()
 
 auth_url, auth_token = f.GetRequestToken()
 
-print 'Please open your web browser and go to the copy the following URL to obtain your pin: %s' %auth_url
+print """
+Please open your web browser and go to the copy the following URL to obtain your pin:
+%s
+""" %auth_url
 
 PIN = raw_input("Please copy the pin that is returned from Fitbit: ")
 
 access_token = f.GetAccessToken(PIN, auth_token)
 
-response = f.ApiCall(access_token, apiCall='/1/user/-/activities/log/steps/date/today/7d.json')
+apistring = f.PickApiCall() 
+
+response = f.ApiCall(access_token, apistring)
+
 print response
