96920cb5cc5163695e352e744d02b6a7388d15da
Rob Havasy <rob@havasy.net>
2013-01-10 14:35:13 -0500

    Added code to open web browser automatically with FitBit auth URL.

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -1,12 +1,9 @@
 # Modified by Alyssa Woulfe to automate the authentication workflow
-import fitbit
+import fitbit, webbrowser
 f=fitbit.FitBit()
 auth_url, auth_token = f.GetRequestToken()
-print """
-Please open your web browser and copy the following URL to obtain your pin:
-%s
-""" %auth_url
-PIN = raw_input("Please copy the pin that is returned from Fitbit: ")
+webbrowser.open(auth_url)
+PIN = raw_input("Please paste the PIN that is returned from Fitbit [ENTER]: ")
 access_token = f.GetAccessToken(PIN, auth_token)
 apistring = f.PickApiCall() 
 response = f.ApiCall(access_token, apistring)
