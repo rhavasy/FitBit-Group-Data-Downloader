e4cc966266f8910d4420265b7d7ac7fb8fa68468
Rob Havasy <rob@havasy.net>
2013-01-10 13:36:02 -0500

    Refactored fitbit_authenticate to remove extra whitespace and corrected typo.

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -1,22 +1,13 @@
 # Modified by Alyssa Woulfe to automate the authentication workflow
-
-
 import fitbit
 f=fitbit.FitBit()
-
 auth_url, auth_token = f.GetRequestToken()
-
 print """
-Please open your web browser and go to the copy the following URL to obtain your pin:
+Please open your web browser and copy the following URL to obtain your pin:
 %s
 """ %auth_url
-
 PIN = raw_input("Please copy the pin that is returned from Fitbit: ")
-
 access_token = f.GetAccessToken(PIN, auth_token)
-
 apistring = f.PickApiCall() 
-
 response = f.ApiCall(access_token, apistring)
-
-print response
+print response #for debugging
