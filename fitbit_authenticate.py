24f2564d507915fe269d7dd0f276e26cd7089c8e
Rob Havasy <rob@havasy.net>
2013-01-10 20:49:19 -0500

    Rewrote authenticate script with function to select and make API call and logic to test for presence of existing token file in default path. Also writes results to XML file.

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -1,10 +1,27 @@
-# Modified by Alyssa Woulfe to automate the authentication workflow
-import fitbit, webbrowser
+# Modified by Alyssa Woulfe to automate the authentication workflow & add file ops
+# Mod by Rob Havasy to test for file existence
+import fitbit, webbrowser, os.path
 f=fitbit.FitBit()
-auth_url, auth_token = f.GetRequestToken()
-webbrowser.open(auth_url)
-PIN = raw_input("Please paste the PIN that is returned from Fitbit [ENTER]: ")
-access_token = f.GetAccessToken(PIN, auth_token)
-apistring = f.PickApiCall() 
-response = f.ApiCall(access_token, apistring)
-print response #for debugging
+
+def MakeApiCall(access_token):
+    apistring = f.PickApiCall() 
+    response = f.ApiCall(access_token, apistring)
+    print response #for debugging
+    file=open('results.xml', 'w') #Write results to file
+    file.write(response)
+    file.close()
+
+if os.path.isfile('access_token.txt'): #First check if file exists
+    file = open('access_token.txt', 'r') #If yes, read token from file
+    access_token=file.read()
+    file.close()
+    MakeApiCall(access_token) #Call for data
+else: #Otherwise, do Oauth dance and reauthenticate   
+    auth_url, auth_token = f.GetRequestToken()
+    webbrowser.open(auth_url)
+    PIN = raw_input("Please paste the PIN that is returned from Fitbit [ENTER]: ")
+    access_token = f.GetAccessToken(PIN, auth_token)
+    file = open('access_token.txt', 'w') #write token to file
+    file.write(access_token)
+    file.close()
+    MakeApiCall(access_token)
