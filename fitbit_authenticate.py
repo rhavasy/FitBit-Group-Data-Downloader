72b904c47c6aaab83303ea4c000f3ad148fea30f
Woulferine <awoulfe@partners.org>
2013-01-11 16:44:41 -0500

    Fixed the file check issue
    Also added the hardcoded name variable to the results.xml file. Still
    working on the final xml read piece.

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -3,18 +3,18 @@
 import fitbit, webbrowser, os.path
 f=fitbit.FitBit()
 
+Name = 'Alyssa'
+
 def MakeApiCall(access_token):
     apistring = f.PickApiCall() 
     response = f.ApiCall(access_token, apistring)
     print response #for debugging
-    file=open('results.xml', 'w') #Write results to file
+    file=open(Name+'_results.xml', 'w') #Write results to file
     file.write(response)
     file.close()
 
-Name = 'Alyssa'
-
-if os.path.isfile(Name+'access_token.txt'): #First check if file exists
-    file = open(Name+'access_token.txt', 'r') #If yes, read token from file
+if os.path.isfile(Name+'_access_token.txt'): #First check if file exists
+    file = open(Name+'_access_token.txt', 'r') #If yes, read token from file
     access_token=file.read()
     file.close()
     MakeApiCall(access_token) #Call for data
@@ -24,13 +24,13 @@ else: #Otherwise, do Oauth dance and reauthenticate
     webbrowser.open(auth_url)
     PIN = raw_input("Please paste the PIN that is returned from Fitbit [ENTER]: ")
     access_token = f.GetAccessToken(PIN, auth_token)
-    file = open(Name+' access_token.txt', 'w') #write token to file
+    file = open(Name+'_access_token.txt', 'w') #write token to file
     file.write(access_token)
     file.close()
     MakeApiCall(access_token)
 
 from xml.etree import ElementTree
-with open('results.xml', 'rt') as f:
+with open(Name+'_results.xml', 'rt') as f:
         tree = ElementTree.parse(f)
 for node in tree.iter('user'):
     name = node.attrib.get('fullname')
