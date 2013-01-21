aefbb5564b31302d776146ebd059da735e043cd8
Alyssa Woulfe <awoulfe@partners.org>
2013-01-21 15:51:32 -0500

    Uses csv to read and write tokens. Amazing.
    Erik helped me out. This should work now. Once config file is complete,
    we can add the csv file name (Program_access_tokens) to the config file
    to read from.

--- fitbit_authenticate.py
+++ fitbit_authenticate.py
@@ -1,33 +1,48 @@
-import fitbit, webbrowser, os.path
+import fitbit, webbrowser, os.path, os, csv
 f=fitbit.FitBit()
-response = ""
-Name = 'Alyssa'
+
+
 
 def MakeApiCall(access_token):
-    apistring = f.PickApiCall() 
+    apistring = f.PickApiCall()
     response = f.ApiCall(access_token, apistring)
-    fo=open(Name+'_results.xml', 'w') #Write results to file
+    fo=open(FileName+'_results.xml', 'w') #Write results to file
     fo.write(response)
     fo.close()
 
-if os.path.isfile(Name+'_access_token.txt'): #First check if file exists
-    fo = open(Name+'_access_token.txt', 'r') #If yes, read token from file
-    access_token=fo.read()
-    fo.close()
-    MakeApiCall(access_token) #Call for data
-else: #Otherwise, do Oauth dance and reauthenticate
-    auth_url, auth_token = f.GetRequestToken()
-    webbrowser.open(auth_url)
-    PIN = raw_input("Please paste the PIN that is returned from Fitbit [ENTER]: ")
-    access_token = f.GetAccessToken(PIN, auth_token)
-    fo = open(Name+'_access_token.txt', 'w') #write token to file
-    fo.write(access_token)
-    fo.close()
-    MakeApiCall(access_token)
+mainfile='MGH_scholars_access_token.csv'
+tmpfile='MGH_scholars_access_token.tmp.csv'
+read_token=open(mainfile,'r')
+csvreader = csv.reader(read_token, dialect='excel', quotechar="'", delimiter=',')
+accesstokensfile = {rows[0]:rows[1] for rows in csvreader}
+access_token = accesstokensfile.values()
+NamesList = accesstokensfile.keys()
+ 
+n=0
+fieldnames = ['value', 'access_token']
+write_token = open(tmpfile, 'wb')
+csvwriter = csv.writer(write_token)
+for value in NamesList:
+    FileName = value
+    print value
+    print access_token[n]
+    try:
+        MakeApiCall(access_token[n])
+        csvwriter.writerow([value, access_token[n]]) 
+    except ValueError:
+        print "Stored access token '" + access_token[n] + "' not accepted for user '" + value + "'"
+        auth_url, auth_token = f.GetRequestToken()
+        webbrowser.open(auth_url)
+        PIN = raw_input("Please paste the PIN that is returned from Fitbit [ENTER]: ")
+        access_token = f.GetAccessToken(PIN, auth_token)
+        csvwriter.writerow([value, access_token])
+        print value
+        print access_token
+        MakeApiCall(access_token)
+    n=n+1
 
-from xml.etree import ElementTree
-with open(Name+'_results.xml', 'rt') as fo:
-        tree = ElementTree.parse(fo)
-for node in tree.iter('user'):
-    name = node.attrib.get('fullname')
-    print name
+write_token.flush()
+write_token.close()
+read_token.close()
+os.remove(mainfile)
+os.rename(tmpfile,mainfile)
